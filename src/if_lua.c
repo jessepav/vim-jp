6b079c0d2f7d1532bc8eef1974430f7594b2717d
Jesse Pavel <jpavel@alum.mit.edu>
2023-08-13 16:39:39 -0400

    Go back to original udata macros

--- src/if_lua.c
+++ src/if_lua.c
@@ -72,22 +72,13 @@ static const char LUA___CALL[] = "__call";
 // most functions are closures with a cache table as first upvalue;
 // get/setudata manage references to vim userdata in cache table through
 // object pointers (light userdata)
-
-    static inline void
-luaV_getudata(lua_State *L, void *v)
-{
-    lua_pushlightuserdata(L, v);
-    lua_rawget(L, lua_upvalueindex(1));
-}
-
-    static inline void
-luaV_setudata(lua_State *L, void *v)
-{
-    lua_pushlightuserdata(L, v);
-    lua_pushvalue(L, -2);
-    lua_rawset(L, lua_upvalueindex(1));
-}
-
+#define luaV_getudata(L, v) \
+    lua_pushlightuserdata((L), (void *) (v)); \
+    lua_rawget((L), lua_upvalueindex(1))
+#define luaV_setudata(L, v) \
+    lua_pushlightuserdata((L), (void *) (v)); \
+    lua_pushvalue((L), -2); \
+    lua_rawset((L), lua_upvalueindex(1))
 #define luaV_getfield(L, s) \
     lua_pushlightuserdata((L), (void *)(s)); \
     lua_rawget((L), LUA_REGISTRYINDEX)
