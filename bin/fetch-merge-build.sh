dbb262dee50ab0412856a04d9e80a822d560cda0
Jesse Pavel <jpavel@alum.mit.edu>
2025-07-25 21:46:55 -0400

    Add --merge-push option

--- bin/fetch-merge-build.sh
+++ bin/fetch-merge-build.sh
@@ -11,10 +11,13 @@ if [[ "$1" == @(-f|--fetch) ]]; then
     [[ -n "$range" ]] && git lg $range || echo "Already up-to-date"
 elif [[ "$1" == @(-m|--merge) ]]; then
     git merge --no-edit upstream/master
+elif [[ "$1" == @(-p|--merge-push) ]]; then
+    git merge --no-edit upstream/master
+    git push
 elif [[ "$1" == @(-b|--build) ]]; then
     cd src
     make && make install
 else
-    echo "Usage: fetch-merge.sh [-f, --fetch] [-m, --merge] [-b, --build]"
+    echo "Usage: fetch-merge.sh [-f, --fetch] [-m, --merge] [-p, --merge-push] [-b, --build]"
     exit
 fi
